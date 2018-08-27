898e48a6d81b521e340e336de860b4fcd0bd2805
Robert Gray <robert.gray@directski.com>
2018-08-27 13:10:48 +0100

    fixed merge issue

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,6 @@
 import com.dbs.utils.SqlLiteHelper;
 import com.dbs.utils.hbase.*;
+import com.dbs.utils.hdfs.StoreCSVFileInHDFS;
 import com.dbs.utils.mapreduce.MapReduceProfanityDriver;
 import com.dbs.utils.mapreduce.MapReduceSongDataDriver;
 
@@ -7,14 +8,14 @@ public class Main {
 
     public static void main(String[] args) throws Exception {
 
-        //StoreCSVFileInHDFS.run();
-        //DeleteSongDataTable.run();
-        //CreateSongDataTable.run();
+        StoreCSVFileInHDFS.run();
+        DeleteSongDataTable.run();
+        CreateSongDataTable.run();
         DeleteSongDataSummaryTable.run();
         CreateSongDataSummaryTable.run();
         DeleteProfanitySummaryTable.run();
         CreateProfanitySummaryTable.run();
-        //AddDataToSongDataTable.run();
+        AddDataToSongDataTable.run();
         MapReduceSongDataDriver.run();
         MapReduceProfanityDriver.run();
         SqlLiteHelper.dropSummaryTable();
